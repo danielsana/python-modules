5b2aece5576a02ca594bf3175ed51257b72316ad
danielsana <danielsana60@gmail.com>
2024-12-13 12:04:24 +0300

    error handling

---
+++ error.py
@@ -0,0 +1,21 @@
+# syntar error 
+
+anount = 1000
+# forgetting a colon 
+# if amount>2999
+#     print("enough money")
+
+
+# exceptions errors 
+# marks = 400
+# a = marks/0
+# print(a)
+
+# using try except 
+
+marks = 400
+try:
+    a = marks/0
+    print(a)
+except Exception as e:
+    print("exception caught", e)
