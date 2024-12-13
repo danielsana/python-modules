f66ccbad9c1368d04ac7cac36aed64915596b431
danielsana <danielsana60@gmail.com>
2024-12-13 11:34:23 +0300

    modules

--- modulesOne.py
+++ modulesOne.py
@@ -1,4 +1,15 @@
-def greet():
-    print('hi')
+# import calc
+# from math import sqrt,factorial
 
-    # hello
+
+# calc.add(56,24)
+# calc.subtract(100,24)
+
+# print(sqrt(16))
+
+
+import requests
+
+response = requests.get("https://api.github.com")
+print(response.status_code)  # Output: 200
+print(response.json())       # Output: API response in JSON format
